341e53a7b1d4338cc317aa4d58959a2c649e677f
DavidMutchler <mutchler@rose-hulman.edu>
2019-09-01 14:33:58 -0400

    adding a file to test PyCharm setup

---
+++ PythonProjects/src/test_my_setup.py
@@ -0,0 +1,3 @@
+print("In PyCharm, right-click in this window and select   'Run test_my_setup'")
+print("If this message shows up in a window, your Python setup is OK.")
+print("Try to commit and push to test that your GIT setup is also OK.")
