12aa59e5104b13ead3f5ed274554a2e00dd1dff8
DavidMutchler <mutchler@rose-hulman.edu>
2019-09-02 18:27:07 -0400

    Made a folder for the test project

--- PythonProjects/00-TestSetup/src/test_my_setup.py
+++ PythonProjects/00-TestSetup/src/test_my_setup.py
@@ -1,3 +1,7 @@
+print()
 print("In PyCharm, right-click in this window and select   'Run test_my_setup'")
 print("If this message shows up in a window, your Python setup is OK.")
-print("Try to commit and push to test that your GIT setup is also OK.")
+print()
+print("Then, add a   print   statement of your own (anything is fine)")
+print("Then, from the VCS menu (at the top), select Commit, then the Commit and Push option")
+print("If you get a message that says your Push was successful, your GIT setup is also OK.")
