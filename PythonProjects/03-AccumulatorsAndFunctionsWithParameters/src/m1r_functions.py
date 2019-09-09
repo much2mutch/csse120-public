8a8c80671f0a58942d903e0d248a77ae0cf6c971
DavidMutchler <mutchler@rose-hulman.edu>
2019-09-09 18:49:32 -0400

    Adding project 03

---
+++ PythonProjects/03-AccumulatorsAndFunctionsWithParameters/src/m1r_functions.py
@@ -0,0 +1,44 @@
+###############################################################################
+#
+# TODO:
+#   1. READ the code below.
+#   2. TRACE (by hand) the execution of the code,
+#        predicting what will get printed.
+#   3. Run the code and compare your prediction to what actually was printed.
+#   4. Decide whether you are 100% clear on the CONCEPTS and the NOTATIONS for:
+#        -- DEFINING a function that has PARAMETERS
+#        -- CALLING a function with actual ARGUMENTS.
+#
+# *****************************************************************************
+#      If you are NOT 100% clear on the above concepts,
+#      ask your instructor or a student assistant about them during class.
+# *****************************************************************************
+#
+# After you have completed the above, mark this _TODO_ as DONE.
+#
+###############################################################################
+
+
+def main():
+    hello("Grace Hopper")
+    goodbye("Ada Lovelace")
+    hello("Mataric")
+    hello("Rus")
+    hello_and_goodbye("One", "Two")
+    hello_and_goodbye("Two", "One")
+
+
+def hello(friend):
+    print("Hello,", friend, "- how are things?")
+
+
+def goodbye(friend):
+    print("Goodbye,", friend, '- see you later!')
+
+
+def hello_and_goodbye(person1, person2):
+    hello(person2)
+    goodbye(person1)
+
+
+main()
