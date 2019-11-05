1eeb421185cf556dc05a87f5553c7934d720cf7d
DavidMutchler <mutchler@rose-hulman.edu>
2019-11-05 09:58:23 -0500

    throwaway, first try

---
+++ PythonProjects/98-Throwaway-202010/src/m0_run_this_on_LAPTOP.py
@@ -0,0 +1,49 @@
+"""
+  Capstone Project.  Code to run on a LAPTOP (NOT the robot).
+  Displays the Graphical User Interface (GUI) and communicates with the robot.
+
+  Authors:  Your professors (for the framework)
+    and PUT_YOUR_NAMES_HERE.
+  Fall term, 2019-2020.
+"""
+# TODO 1:  Put the name of EACH team member in the above.
+
+import tkinter
+from tkinter import ttk
+
+import mqtt_remote_method_calls as mqtt
+import m1_laptop_code as m1
+import m2_laptop_code as m2
+import m3_laptop_code as m3
+
+
+def main():
+    """
+    This code, which must run on a LAPTOP:
+      1. Constructs and displays a GUI for teleoperation and EACH team
+         member's part of the Capstone Project.
+      2. Communicates via MQTT with the code that runs on the EV3 robot.
+    """
+    # -------------------------------------------------------------------------
+    # TODO: The root TK object for the GUI:
+    # -------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------
+    # TODO: The main frame, upon which the other frames are placed:
+    #   Give it a fun title (keep it G-rated!)
+    # -------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------
+    # TODO: Each team member has their own frame for their own GUI.
+    #   Get them from each mX.get_my_frame(), and grid them left to right.
+    # -------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------
+    # TODO: The event loop:
+    # -------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# -----------------------------------------------------------------------------
+main()
---
+++ PythonProjects/98-Throwaway-202010/src/m2_laptop_code.py
@@ -0,0 +1,33 @@
+"""
+  Capstone Project.  Code to run on a LAPTOP (NOT the robot).
+  Displays the Graphical User Interface (GUI) and communicates with the robot.
+
+  Authors:  Your professors (for the framework)
+    and PUT_YOUR_NAME_HERE.
+  Fall term, 2019-2020.
+"""
+# TODO 1:  Put your name in the above.
+
+import tkinter
+from tkinter import ttk
+
+import mqtt_remote_method_calls as mqtt
+import m2_laptop_code as m2
+import m3_laptop_code as m3
+
+
+def get_my_frame(root):
+    """
+    Constructs and returns a ttk.Frame with all my GUI widgets on it.
+      :type root: tkinter.Tk
+    """
+    # TODO: Construct your frame, putting it on the root.
+    #   Put, onto your frame, a label that states your name:
+
+    # TODO: Put the rest of your GUI onto your frame
+    #  (using sub-frames if you wish):
+
+    # TODO: Return your frame:
+
+
+# TODO: Add functions here as needed.
