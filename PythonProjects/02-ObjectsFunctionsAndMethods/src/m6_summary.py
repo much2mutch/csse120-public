fb93897c1509380422a46649b02fc050dbfb8195
DavidMutchler <mutchler@rose-hulman.edu>
2019-12-03 22:46:51 -0500

    New exercise that summarizes defining functions and calling functions and methods.

---
+++ PythonProjects/02-ObjectsFunctionsAndMethods/src/m6_summary.py
@@ -0,0 +1,30 @@
+"""
+An exercise that summarizes what you have learned in this Session.
+
+Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
+         Derek Whitley, their colleagues, and PUT_YOUR_NAME_HERE.
+"""
+###############################################################################
+# TODO: 1.
+#   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
+###############################################################################
+
+###############################################################################
+# TODO: 2.
+#   Define a complete program that:
+#     a.  Imports  rosegraphics
+#     b.  Defines a   main   function that:
+#          - Constructs a TurtleWindow.
+#          - Calls the function that YOU define (see next bullet, below) TWICE
+#              (with different arguments each time) to TEST your function.
+#          - Asks the TurtleWindow to wait for a mouse click.
+#     c.  Defines another function that takes three parameters:
+#             a SimpleTurtle, a string that represents a color,
+#             and an integer for the distance to move (in pixels),
+#         and causes the SimpleTurtle to:
+#           - Move backward the given distance.
+#           - Change its Pen's color to the given color.
+#           - Turn left 90 degrees.
+#           - Move forward twice the given distance.
+#     d.  Calls main.
+###############################################################################
