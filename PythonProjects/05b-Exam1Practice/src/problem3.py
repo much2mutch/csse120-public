8be58f10f3227e0da105d3a437121e756e42e65f
DavidMutchler <mutchler@rose-hulman.edu>
2019-12-09 15:53:08 -0500

    Fixed typo in doc-comment

--- PythonProjects/05b-Exam1Practice/src/problem3.py
+++ PythonProjects/05b-Exam1Practice/src/problem3.py
@@ -39,7 +39,7 @@ def run_test_problem3a():
     """ Tests the   problem3a   function. """
     # -------------------------------------------------------------------------
     # TODO: 2. Implement this TEST function.
-    #   It TESTS the  problem1a  function defined below.
+    #   It TESTS the  problem3a  function defined below.
     #   Include at least **   5   ** tests (we wrote four for you).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
