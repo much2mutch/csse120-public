a06497a2f7b16eb9f1c940bf653d5efba28d3751
DavidMutchler <mutchler@rose-hulman.edu>
2020-02-10 03:56:15 -0500

    Put an explanation of what to do.

--- PythonProjects/97-Throwaway-202020/main.py
+++ PythonProjects/97-Throwaway-202020/main.py
@@ -1,2 +1,5 @@
-import labs.lab1_drive_system
-import labs.lab2a_touch_sensor
+# Un-comment the line for your Lab to run the tests.
+# Do NOT commit this file, since it will lead to CONFLICTS with teammates.
+
+# import labs.lab1_drive_system
+# import labs.lab2a_touch_sensor
