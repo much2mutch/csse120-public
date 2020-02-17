8a944b7f7b297bc7d1ee694d66502318aedd1453
David Fisher <fisherds@rose-hulman.edu>
2020-02-17 01:09:13 -0500

    created the chunk c solution code

---
+++ PythonProjects/99-CapstoneProject-202020c_SOLUTION/labs/lab3_digital_inputs.py
@@ -0,0 +1,197 @@
+"""
+Capstone Team Project.  Code to run on a ROBOT (NOT a laptop).
+
+This module is for testing STAND-ALONE code running on the ROBOT
+(WITHOUT having LAPTOP GUI code running on the LAPTOP at the same time).
+
+Authors:  Your professors (for the framework)
+    and PUT_YOUR_NAME_HERE.
+Winter term, 2019-2020.
+"""
+# TODO: 1.  Put your name in the above.
+
+import rosebot
+import time
+
+
+def main():
+    """ Test a robot's Remote Control, Brick Buttons, and LEDs. """
+    print()
+    print('--------------------------------------------------')
+    print(' Testing Remote Control, Brick Buttons, and LEDs')
+    print('--------------------------------------------------')
+
+    # -------------------------------------------------------------------------
+    # TODO: 2. Construct a robot, that is, a rosebot.RoseBot() object.
+    # -------------------------------------------------------------------------
+    robot = rosebot.RoseBot()
+
+    # -------------------------------------------------------------------------
+    # STUDENTS: Do the work in this module as follows.
+    #   Otherwise, you will be overwhelmed by the number of tests happening.
+    #
+    #   For each function that you implement:
+    #     1. Locate the statements just below this comment that call TEST functions.
+    #     2. UN-comment only one test at a time.
+    #     3. Implement that function per its _TODO_.
+    #     4. Implement as needed the appropriate class methods
+    #     5. When satisfied with your work, move onto the next test,
+    #        RE-commenting out the previous test to reduce the testing.
+    # -------------------------------------------------------------------------
+
+    # run_test_leds_on_off(robot)
+    # run_test_leds_colors(robot)
+    # run_test_brick_buttons(robot)
+    run_test_remote_control(robot)
+
+
+def run_test_leds_on_off(robot):
+    """
+    Tests the  set_color and  turn_off    methods of the Leds class.
+    """
+    print('--------------------------------------------------')
+    print('Testing the  turn_on  turn_off    methods of the robot')
+    print('--------------------------------------------------')
+    while True:
+        # -------------------------------------------------------------------------
+        # TODO: 3. Call the  set_color  and  turn_off methods on the   leds   of the robot,
+        #  so that "both" LEDs are on "red" for 1 second, then off for 1 second (repeating forever)
+        #  In addition to the LEDs print to the console "Both LEDs On as Red" or "LEDs Off"
+        #  just before the LED change line of code (so you can see it and read it).
+        # -------------------------------------------------------------------------
+
+        # Solution to be removed
+        print("Both LEDs On as Red")
+        robot.leds.set_color("both", "red")
+        time.sleep(1.0)
+
+        print("Both LEDs Off")
+        robot.leds.turn_off()
+        time.sleep(1.0)
+
+
+def run_test_leds_colors(robot):
+    """
+    Tests the  set_color_by_name    methods of the ArmAndClaw class.
+    """
+    print('--------------------------------------------------')
+    print('Testing the  set_color   methods of the robot')
+    print('--------------------------------------------------')
+    left_colors = ["red", "green", "amber", "off", "red", "off", "green", "off", "amber", "off"]
+    right_colors = ["red", "green", "amber", "off", "off", "red", "off", "green", "off", "amber"]
+
+    # -------------------------------------------------------------------------
+    # TODO: 4. Call the  set_color  method of the   leds   of the robot,
+    #  so that the color changes through each value in the lists above.
+    #  Instead of using a 1 second delay between colors, have the user hit
+    #  the enter key to change to the next colors.
+    #
+    # After the final value in the list, the program should turn off both LEDs and end.
+    # Notice that this _TODO_ is not within a while True loop (unlike other parts)
+    # -------------------------------------------------------------------------
+
+    # Solution to be removed
+    for k in range(len(left_colors)):
+        robot.leds.set_color("left", left_colors[k])
+        robot.leds.set_color("right", right_colors[k])
+        input("Press the ENTER key to change to the next color.")
+
+    robot.leds.turn_off()
+
+
+def run_test_brick_buttons(robot):
+    """
+    Tests the  brick_buttons    methods of the BrickButton class.
+    """
+    print('--------------------------------------------------')
+    print('Testing the  brick_buttons   methods of the robot')
+    print('--------------------------------------------------')
+    while True:
+        time.sleep(0.05)
+
+        # -------------------------------------------------------------------------
+        # TODO: 5. Create a small program that will light the appropriate LEDs
+        #   when buttons on the EV3 Brick are pressed:
+        #    When up is pressed light both LEDs green
+        #    When down is pressed light both LEDs red
+        #    When left is pressed light the left LED amber
+        #    When right is pressed light the right LED amber
+        #    When backspace is pressed break from the loop and end the program
+        #    When no button is pressed turn the LEDs off
+        #
+        #  Note, only 1 button will be pressed at a time.
+        # -------------------------------------------------------------------------
+
+        # Solution to be removed
+        if robot.brick_buttons.is_up_pressed():
+            robot.leds.set_color("both", "green")
+        elif robot.brick_buttons.is_down_pressed():
+            robot.leds.set_color("both", "red")
+        elif robot.brick_buttons.is_left_pressed():
+            robot.leds.set_color("left", "amber")
+        elif robot.brick_buttons.is_right_pressed():
+            robot.leds.set_color("right", "amber")
+        elif robot.brick_buttons.is_backspace_pressed():
+            print("Goodbye")
+            break
+        else:
+            robot.leds.turn_off()
+
+
+def run_test_remote_control(robot):
+    """
+    Tests the  remote_control    methods of the RemoteControl class.
+    """
+    print('--------------------------------------------------')
+    print('Testing the  remote_control   methods of the robot')
+    print('--------------------------------------------------')
+
+    speed = 50
+    while True:
+        time.sleep(0.05)
+
+        # -------------------------------------------------------------------------
+        # TODO: 6. Create a small program that will move the motors of the robot as follows
+        #   when buttons on the remote control are pressed:
+        #    When channel 1 red up is pressed drive the left motor forward at speed
+        #    When channel 1 red down is pressed drive the left motor backwards at -speed
+        #         If neither channel 1 red up or red down is pressed the left motor should stop
+        #    When channel 1 blue up is pressed drive the right motor forward at speed
+        #    When channel 1 blue down is pressed drive the right motor backwards at -speed
+        #         If neither channel 1 blue up or blue down is pressed the right motor should stop
+        #    When channel 2 red up is pressed raise the arm
+        #    When channel 2 red down is pressed lower the arm (note, a calibration is needed first)
+        #    When channel 2 blue up is pressed calibrate the arm
+        #    When backspace is pressed on the EV3 break from the loop and end the program
+        #
+        # Note, MULTIPLE buttons may be pressed at the same time in the program.
+        # -------------------------------------------------------------------------
+
+        # Solution to be removed
+        if robot.remote_control.is_pressed(1, "red_up"):
+            robot.drive_system.left_motor.turn_on(speed)
+        elif robot.remote_control.is_pressed(1, "red_down"):
+            robot.drive_system.left_motor.turn_on(-speed)
+        else:
+            robot.drive_system.left_motor.turn_off()
+
+        if robot.remote_control.is_pressed(1, "blue_up"):
+            robot.drive_system.right_motor.turn_on(speed)
+        elif robot.remote_control.is_pressed(1, "blue_down"):
+            robot.drive_system.right_motor.turn_on(-speed)
+        else:
+            robot.drive_system.right_motor.turn_off()
+
+        if robot.remote_control.is_pressed(2, "red_up"):
+            robot.arm_and_claw.raise_arm()
+        if robot.remote_control.is_pressed(2, "red_down"):
+            robot.arm_and_claw.lower_arm()
+        if robot.remote_control.is_pressed(2, "blue_up"):
+            robot.arm_and_claw.calibrate_arm()
+
+        if robot.brick_buttons.is_backspace_pressed():
+            print("Goodbye")
+            break
+
+
+main()
---
+++ PythonProjects/99-CapstoneProject-202020c_SOLUTION/labs/lab6_camera.py
@@ -0,0 +1,133 @@
+"""
+Capstone Team Project.  Code to run on a ROBOT (NOT a laptop).
+
+This module is for testing STAND-ALONE code running on the ROBOT
+(WITHOUT having LAPTOP GUI code running on the LAPTOP at the same time).
+
+Authors:  Your professors (for the framework)
+    and PUT_YOUR_NAME_HERE.
+Winter term, 2019-2020.
+"""
+# TODO: 1.  Put your name in the above.
+
+import time
+import rosebot
+
+
+def main():
+    """ Test a robot's camera. """
+    print()
+    print('--------------------------------------------------')
+    print('Testing the Camera of a robot')
+    print('--------------------------------------------------')
+
+    # -------------------------------------------------------------------------
+    # TODO: 2. Construct a robot, that is, a rosebot.RoseBot() object.
+    #          Then  ** ENABLE ITS CAMERA. **
+    # -------------------------------------------------------------------------
+    robot = rosebot.RoseBot()
+
+    # -------------------------------------------------------------------------
+    # STUDENTS: Do the work in this module as follows.
+    #   Otherwise, you will be overwhelmed by the number of tests happening.
+    #
+    #   For each function that you implement:
+    #     1. Locate the statements just below this comment that call TEST functions.
+    #     2. UN-comment only one test at a time.
+    #     3. Implement that function per its _TODO_.
+    #     4. Implement as needed the appropriate class methods
+    #     5. When satisfied with your work, move onto the next test,
+    #        RE-commenting out the previous test to reduce the testing.
+    # -------------------------------------------------------------------------
+    robot.camera.enable()
+
+    # run_test_blob_readings(robot)
+    # run_test_spin_until_color_seen(robot)
+    run_test_spin_to_track_color(robot)
+
+
+def run_test_blob_readings(robot):
+    """
+    Tests the   get_biggest_blob   methods of the Camera  class.
+    """
+    print('--------------------------------------------------')
+    print('Testing the  get_biggest_blob   method of the camera of the robot')
+    print('--------------------------------------------------')
+    while True:
+        time.sleep(1.0)
+        # -------------------------------------------------------------------------
+        # TODO: 8. Use the  get_biggest_blob  method of the  camera
+        #  of the robot to display Blob readings once per second.
+        #  Print the blob (it has a __repr__ method so it can be printed)
+        # -------------------------------------------------------------------------
+
+        # Solution to be removed
+        blob = robot.camera.get_biggest_blob()
+        print(blob)
+
+
+def run_test_spin_until_color_seen(robot):
+    """
+    Tests the  spin_until_color_seen    method of the   class.
+    :type robot: rosebot.RoseBot
+    """
+    print('--------------------------------------------------')
+    print('Testing the   spin_until_color_seen  method of the robot')
+    print('--------------------------------------------------')
+    while True:
+        print()
+        speed = int(input("Enter an integer for the max wheel speed (1 to 100): "))
+        if speed == 0:
+            break
+        blob_area_threshold = int(input("What is your Blob area threshold? (10000 for a 100x100) "))
+        if blob_area_threshold == 0:
+            break
+        input("Press the ENTER key when ready for the robot to start moving.")
+
+        # -------------------------------------------------------------------------
+        # TODO: 10. Call the  spin_until_color_seen  method of the   camera_tracker
+        #  of the robot passing in the blob_area_threshold.
+        #  Once the target is found make the robot beep.
+        # -------------------------------------------------------------------------
+
+        # Solution to be removed
+        robot.camera_tracker.spin_until_color_seen(speed, blob_area_threshold)
+        robot.sound.beep()
+
+
+def run_test_spin_to_track_color(robot):
+    """
+    Tests the  spin_until_beacon_seen and spin_to_track_beacon    methods of the   class.
+    :type robot: rosebot.RoseBot
+    """
+    print('--------------------------------------------------')
+    print('Testing the spin_to_track_beacon method of the BeaconSeeker')
+    print('--------------------------------------------------')
+    while True:
+        print()
+        speed = int(input("Enter an integer for the max wheel speed (1 to 100): "))
+        if speed == 0:
+            break
+        tracking_duration_s = int(input("How long would you like to track the color (seconds)? "))
+        if tracking_duration_s == 0:
+            break
+        input("Press the ENTER key when ready for the robot to start moving.")
+
+        # -------------------------------------------------------------------------
+        # TODO: 11. Call the  spin_to_track_beacon  method of the  beacon_seeker
+        #  of the robot passing in the tracking_duration_s
+        #  Once the tracking_duration_s is over make the robot beep.
+        # Info:
+        #  - The heading is in degrees in the range -25 to 25 with:
+        #      - 0 means straight ahead
+        #      - negative degrees mean the Beacon is to the left
+        #      - positive degrees mean the Beacon is to the right
+        #  - Distance is from 0 to 100, where 100 is about 70 cm
+        # -------------------------------------------------------------------------
+
+        # Solution to be removed
+        robot.camera_tracker.spin_to_track_color(speed, tracking_duration_s)
+        robot.sound.beep()
+
+
+main()
