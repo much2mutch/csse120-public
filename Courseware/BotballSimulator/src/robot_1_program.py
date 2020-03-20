7e9c4cfbe55a20b28d26e8e91864175c846e1bc3
DavidMutchler <mutchler@rose-hulman.edu>
2020-03-20 11:45:21 -0400

    first stab

---
+++ Courseware/BotballSimulator/src/robot_1_program.py
@@ -0,0 +1,10 @@
+from simulation_robot import *
+
+
+# Put your Botball program below here.  The main function
+# (where your program starts) must be called   main.
+def main():
+    motor(0, 100)
+    motor(1, -100)
+    msleep(2000)
+    ao()
