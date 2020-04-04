fb4df16d25e5d3b6603d6959929137d46d0aad2e
DavidMutchler <mutchler@rose-hulman.edu>
2020-04-04 16:39:59 -0400

    inconsequential changes to an example

--- PythonProjects/29-Tkinter/more_examples/m8e_images.py
+++ PythonProjects/29-Tkinter/more_examples/m8e_images.py
@@ -23,16 +23,17 @@ def main():
     main_frame = ttk.Frame(root, padding=20)
     main_frame.grid()
 
-    # The image file must be GIF (or one of several other unhelpful
-    # formats). To convert a JPG or anything else, use an outside tool.
+    # The image file must be GIF or PNG (or one of several other unhelpful
+    # formats). To convert a JPG or anything else, use an outside tool
+    # or install the PILLOW package and use it.
     # Note that the image file must be in the same folder as this
     # module, if you use this way to refer to the image file.
-    photo = tkinter.PhotoImage(file='tkinter_mqtt_ev3.gif')
+    photo = tkinter.PhotoImage(file='image1.gif')
 
     button1 = ttk.Button(main_frame, image=photo)
-    # The next line is necessary when your root.mainloop() call is outside the 
-    # current method. Tkinter has a bug that makes it think the image is ready for 
-    # garbage collection, which makes the image fully transparent. 
+    # The next line is necessary when your root.mainloop() call is outside the
+    # current method. Tkinter has a bug that makes it think the image is ready
+    # for garbage collection, which makes the image fully transparent.
     # See: http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
     button1.image = photo
     button1.grid()
